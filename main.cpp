2c9c5d574d59b0b0b6509ffc37b1fd84db605ac1
rider <rider@DESKTOP-12DN3E3.localdomain>
2026-05-08 22:48:02 +0000

    changes

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  cout << "Value received: " << n << endl;
+  cout << "Priyanshu Walia" << endl;
+  cout << "finally this shit works" << endl;
+  cout << "lets see if it prints this";
+  cout << "We will do DSA IN here from now on does this sound good?";
+  return 0;
+}
